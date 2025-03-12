e737085cb9f6512e9f4ba9d66a17db13a5622088
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-12 11:51:16 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,6 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     const heroTitle = document.querySelector(".hero-title");
     const heroSubtitle = document.querySelector(
+
+
+    // Initial Animation
+    setTimeout(() => {
+        heroTitle.style.opacity = '1';
+        heroTitle.style.transform = 'translateY(0)';
+        heroSubtitle.style.opacity = '1';
+        heroSubtitle.style.transform = 'translateY(0)';
+    }, 500);
     
     
     
