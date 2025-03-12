bee183487b0a774f2a719baf9b8c5bd6bc1c958f
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-12 11:49:57 -0400

    update script.js

---
+++ Script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const heroTitle = document.querySelector(".hero-title");
+    const heroSubtitle = document.querySelector(
+    
+    
+    
+    
+    }
